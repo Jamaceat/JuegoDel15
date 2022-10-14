59f68fc2087b403d05f5d0a0d5d08d05ec44a3c3
Johan <jamacea@uninorte.edu.co>
2022-10-13 23:14:32 -0500

    Eliminando codigo no utilizado y agregando una descripcion del input random

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,28 +6,7 @@ const nPasosListo = document.querySelector("#preparado")
 const npasos = document.querySelector("#npasos")
 const winner = document.querySelector("#Ganador")
 let jugando = false
-// function shuffle(array) {
-// 	let currentIndex = array.length
-// 	let randomIndex
-// 	// While there remain elements to shuffle.
-// 	while (currentIndex != 0) {
-// 		// Pick a remaining element.
-// 		randomIndex = Math.floor(Math.random() * currentIndex)
-// 		currentIndex--
 
-// 		// And swap it with the current element.
-// 		;[array[currentIndex], array[randomIndex]] = [
-// 			array[randomIndex],
-// 			array[currentIndex],
-// 		]
-// 	}
-
-// 	return array
-// }
-
-// nPasosListo.addEventListener("click", (e) => {
-
-// })
 nPasosListo.addEventListener("click", (e) => {
 	if (Number(npasos.value) > 0 && Number(npasos.value) <= 1000) {
 		revolver(Number(npasos.value))
